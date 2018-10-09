25e96b8bd4f7e6569841e2bab3ccb8eb34a073a3
Ravenna Souto <43768185+annevaR@users.noreply.github.com>
2018-10-09 02:20:39 -0300

    Update Horario.java

--- Horario.java
+++ Horario.java
@@ -1,12 +1,12 @@
 package projeto;
 
 public class Horario  {
-    private Disciplina disciplina; 
-    private Docente professor;
-    private Laboratorio laboratorio;
+    private Disciplina[] disciplina; 
+    private Docente[] professor;
+    private Laboratorio[] laboratorio;
           
    
-    public Horario(Disciplina disciplina, Docente  professor, Laboratorio laboratorio) {
+    public Horario(Disciplina[] disciplina, Docente[]  professor, Laboratorio[] laboratorio) {
         this.disciplina = disciplina;
         this.professor = professor;
         this.laboratorio = laboratorio;
@@ -17,27 +17,27 @@ public class Horario  {
         
     }
 
-    public Disciplina  getDisciplina() {
+    public Disciplina[]  getDisciplina() {
         return this.disciplina;
     }
 
-    public void setDisciplina(Disciplina disciplina) {
+    public void setDisciplina(Disciplina[] disciplina) {
         this.disciplina = disciplina;
     }
 
-    public Docente getProfessor() {
+    public Docente[] getProfessor() {
         return this.professor;
     }
 
-    public void setProfessor(Docente professor) {
+    public void setProfessor(Docente[] professor) {
         this.professor = professor;
     }
 
-    public Laboratorio getLaboratorio() {
+    public Laboratorio[] getLaboratorio() {
         return this.laboratorio;
     }
 
-    public void setLaboratorio(Laboratorio laboratorio) {
+    public void setLaboratorio(Laboratorio[] laboratorio) {
         this.laboratorio = laboratorio;
     }
 
