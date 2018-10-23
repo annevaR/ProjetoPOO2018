f183e46576de9475ffc417b242f3a1e488fe6d73
Ravenna Souto <43768185+annevaR@users.noreply.github.com>
2018-10-23 16:36:48 -0300

    Etapa 2 - Relacionamentos entre Classes

--- GradeDeHorarios.java
+++ GradeDeHorarios.java
@@ -1,90 +1,22 @@
 package projeto;
 
-import java.util.ArrayList;
-
 public class GradeDeHorarios {
-     private String[][] grade;
-     ArrayList<ArrayList<Horario>> matrizHorario;
-     int i;
-
-
-    public GradeDeHorarios() {
-        this.grade = new String[6][5]; {
+    int i;
+    int d;
+     
+        Horario[][] gradeDeHorarios = new Horario[6][5]; {
         
-        grade[0][0] = " ";
-        grade[0][1] = " ";
-        grade[0][2]= " ";
-        grade[0][3]= " ";
-        grade[0][4]= " ";
- 
-        
-        grade[1][0] = " ";
-        grade[1][1] = " ";
-        grade[1][2] = " ";
-        grade[1][3] = " ";
-        grade[1][4] = " ";
-    
-         
-        grade[2][0] = " ";
-        grade[2][1] = " ";
-        grade[2][2] = " ";
-        grade[2][3] = " ";
-        grade[2][4] = " ";
-    
-        
-        grade[3][0] = " ";
-        grade[3][1] = " ";
-        grade[3][2] = " ";
-        grade[3][3] = " ";
-        grade[3][4] = " ";
-    
-
-        grade[4][0] = " ";
-        grade[4][1] = " ";
-        grade[4][2] = " ";
-        grade[4][3] = " ";
-        grade[4][4] = " ";
-      
-        
-        grade[5][0] = " ";
-        grade[5][1] = " ";
-        grade[5][2] = " ";
-        grade[5][3] = " ";
-        grade[5][4] = " ";
-        
-        
-            for (i = 0; i< grade.length; i++) {
-                for (int d = 0; d<grade.length; d++) {
-                    System.out.println(grade[i][d] + " ");
+            for (i = 0; i< gradeDeHorarios.length; i++) {
+                for (d = 0; d < gradeDeHorarios.length; d++) {
+                    System.out.println(gradeDeHorarios[i][d] + " ");
                 }
             }
         }
-    }
-
-    public void addHorario(ArrayList<Horario> i) {
-        this.matrizHorario.add(i);
-    }
-
-    public ArrayList<Horario> remove(int i) {
-        return matrizHorario.remove(i);
+    
+    
+    public void addHorario(int dia, Horario h){
+         this.gradeDeHorarios[h.getAula()][dia] = h;
+    
     }
     
-    public String[][] getGrade() {
-        return this.grade;
-    }
-
-    public void setGrade(String[][] grade) {
-        this.grade = grade;
-    }         
-
-    public ArrayList<ArrayList<Horario>> getMatrizHorario() {
-        return matrizHorario;
-    }
-
-    public void setMatrizHorario(ArrayList<ArrayList<Horario>> matrizHorario) {
-        this.matrizHorario = matrizHorario;
-    }
-
-    
-
 }
